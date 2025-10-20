398c16a4a7c4b2d2912eed4f8d456328f5da5fc3
Kirill Shirobokov <ildrekli@gmail.com>
2025-10-20 10:23:05 +0300

    Initial commit + laravel install

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import {defineConfig} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'booking.test',
+            clientPort: 5173
+        },
+        watch: {
+            usePolling: true,
+        }
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            refresh: true,
+        }),
+        tailwindcss(),
+        vue(),
+    ],
+});
